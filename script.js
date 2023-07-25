ed8dd3701d01dbfebed462cc2c0ceff3587adffe
MaheshMERN <88592368+MaheshMERN@users.noreply.github.com>
2023-07-25 17:07:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let name = document.getElementById("form1")["fname"].value;
+	let sirname = document.getElementById("form1")["lname"].value
+alert(`${name} ${sirname}`)
 }
